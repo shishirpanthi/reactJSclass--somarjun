ef950e3251982864010c9cc2abdd3c774aa90414
shishir panthi <shishir12312@gmail.com>
2025-10-29 12:26:36 +0545

    clas 5

--- react/class/src/components/C/C.jsx
+++ react/class/src/components/C/C.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { FaUsers, FaHeart, FaStar, FaBell } from "react-icons/fa";
+import data from "./data.json";
+import styles from "./DashboardCard.module.css";
 
-function C() {
+const icons = [FaUsers, FaHeart, FaStar, FaBell];
+
+const DashboardCard = () => {
   return (
-    <div>
-      
+    <div className={styles.cardContainer}>
+      {data.map((item, index) => {
+        const Icon = icons[index];
+        return (
+          <div key={index} className={styles.card}>
+            <Icon className={styles.icon} />
+            <h2 className={styles.count}>{item.count}</h2>
+            <p className={styles.description}>{item.description}</p>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default C
+export default DashboardCard;
